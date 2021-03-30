8bdbb747f9c03ae517481eea1f09535be80df1c2
vietlqgch18109 <69781453+vietlqgch18109@users.noreply.github.com>
2021-03-30 21:23:26 +0700

    e

--- EnterpriseWeb/login.php
+++ EnterpriseWeb/login.php
@@ -16,9 +16,9 @@
   <title>Login page</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+  <link href="vendor1/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor1/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="vendor1/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
   <link href="css/login.css" rel="stylesheet">
 
--- EnterpriseWeb/manage-coordinator.php
+++ EnterpriseWeb/manage-coordinator.php
@@ -70,11 +70,11 @@ $error = $msg = "";
     <title>Admin Page</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor1/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="vendor1/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="vendor1/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet"
         type="text/css">
 
