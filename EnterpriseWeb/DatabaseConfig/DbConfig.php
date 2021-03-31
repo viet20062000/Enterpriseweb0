edb47f2729a086619917ce9721690f3b7247080d
vietlqgch18109 <69781453+vietlqgch18109@users.noreply.github.com>
2021-03-31 15:57:46 +0700

    Update DbConfig.php

--- EnterpriseWeb/DatabaseConfig/DbConfig.php
+++ EnterpriseWeb/DatabaseConfig/DbConfig.php
@@ -1,10 +1,16 @@
 <?php
-$servername="localhost";
-$DBusername="root";
-$DBpass="";
-$DBname="comp1640";
 
-$conn= mysqli_connect($servername,$DBusername,$DBpass,$DBname);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 if (!$conn){
     die("Connection failed: ".mysqli_connect_error());
 }
